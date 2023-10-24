c7daca2d86396fe5ef1d150c207c069d93944e44
dark-le <ilyascb25@gmail.com>
2023-10-24 23:52:23 +0100

    v-beta

--- js.js
+++ js.js
@@ -1,38 +1,70 @@
-
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
 // get the color mode
 var mode = document.getElementById('color_mode');
 mode.onclick = function () {
     if (mode.innerText == "light_mode") {
         mode.innerText = "dark_mode";
-        $(":root").css('--main-bgcoloralph', "hsla(229, 100%, 50%, 1)")
+        $(":root").css('--main-bgcoloralph', "rgb(133, 255, 198)")
         $(":root").css('--main-bgcolor', "rgb(0, 0, 0)")
         $(":root").css("--main-color", "rgb(255, 255, 255)")
+        $(":root").css("--main-url", "url(./assite/backgroun6.jpg)")
     } else if (mode.innerText == "dark_mode") {
         mode.innerText = "light_mode";
-        $(":root").css('--main-bgcoloralph', "hsla(229, 100%, 50%, 1)")
+        $(":root").css('--main-bgcoloralph', "rgb(133, 255, 198)")
         $(":root").css('--main-bgcolor', "rgb(255, 255, 255)")
         $(":root").css("--main-color", "rgb(0, 0, 0)")
+        $(":root").css("--main-url", "url(./assite/backgroundark5.jpg)")
     }
 }
-//fdsf
-let form = document.getElementById('form');
-let city = document.getElementById('city');
-let when_date_in = document.getElementById('when_date_in');
-let when_date_out = document.getElementById('when_date_out');
-let number_adult = document.getElementById('number_adult');
-let number_children = document.getElementById('number_children');
-let list_id = ['city', 'when_date_in', 'when_date_out', 'number_adult', 'number_children'];
+// 
+var string = `
+Welcome to my developer portfolio. I'm Ilias Aitouchen, a passionate full-stack developer
+with a strong background in web development. I'm committed to delivering efficient and
+maintainable code, and I thrive on problem-solving. My goal is to create innovative
+solutions
+and make ideas come to life.
+               `; /* type your text here */
+var array = string.split("");
+var timer;
 
-list_id.forEach(element => {
-    document.getElementById(element);
+function frameLooper() {
+    if (array.length > 0) {
+        document.getElementById("text").innerHTML += array.shift();
+    } else {
+        clearTimeout(timer);
+    }
+    loopTimer = setTimeout('frameLooper()', 50); /* change 70 for speed */
+
+}
+frameLooper();
+var names = ["i'm a full - stack developer", "i'm a forntend developer", "i'm a backend developer"]
+var i = 0;
+function frametitle() {
+    if (i >= names.length) {
+        i = 0
+    }
+    document.getElementById("title").innerHTML = names[i];
+    i++
+    setTimeout('frametitle()', 2000); /* change 70 for speed */
+}
+frametitle()
+// ---------download
+document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the button element
+    const downloadButton = document.getElementById("downloadButton");
+
+    // Add a click event listener to the button
+    downloadButton.addEventListener("click", function () {
+        // Define the file URL you want to download
+        const fileUrl = "./assite/aitouchenIlias.pdf"; // Replace with the actual file URL
+
+        // Create an invisible anchor element to trigger the download
+        const link = document.createElement("a");
+        link.href = fileUrl;
+
+        // Specify the filename for the downloaded file
+        link.download = "aitouchenIlias.pdf"; // Replace with your desired file name
+
+        // Trigger a click event on the anchor element to initiate the download
+        link.click();
+    });
 });
